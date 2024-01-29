6863167be889d6c183a9700120c485d95742cc5f
ervitis <victor.martinalonso@hrbrain.co.jp>
2024-01-29 22:51:44 +0900

    fix link

--- infra/network.tf
+++ infra/network.tf
@@ -9,7 +9,7 @@ resource "google_compute_subnetwork" "internal-vpc-subnet" {
   name                     = "int-vpc-subnet"
   ip_cidr_range            = "10.5.5.0/24"
   region                   = var.region
-  network                  = google_compute_subnetwork.internal-vpc-subnet.self_link
+  network                  = google_compute_network.internal-vpc-network.self_link
   project                  = var.project_id
   private_ip_google_access = true
 }
