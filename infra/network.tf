203f73067d66511e80fe958d471c46d0eda2c6aa
ervitis <victor.martinalonso@hrbrain.co.jp>
2024-01-29 23:52:40 +0900

    fix delete vpc

--- infra/network.tf
+++ infra/network.tf
@@ -5,24 +5,24 @@ resource "google_compute_network" "internal-vpc-network" {
   project                 = var.project_id
 }
 
-resource "google_vpc_access_connector" "internal-vpc-connector" {
-  name           = "vpc-connector"
-  network        = google_compute_network.internal-vpc-network.id
-  region         = var.region
-  ip_cidr_range  = "10.5.5.0/28"
-  project        = var.project_id
-  min_throughput = 200
-  max_throughput = 500
-
-  min_instances = 2
-  max_instances = 4
-
-  machine_type = "e2-micro"
-
-  depends_on = [
-    google_project_service.network_apis
-  ]
-}
+# resource "google_vpc_access_connector" "internal-vpc-connector" {
+#   name           = "vpc-connector"
+#   network        = google_compute_network.internal-vpc-network.id
+#   region         = var.region
+#   ip_cidr_range  = "10.5.5.0/28"
+#   project        = var.project_id
+#   min_throughput = 200
+#   max_throughput = 500
+#
+#   min_instances = 2
+#   max_instances = 4
+#
+#   machine_type = "e2-micro"
+#
+#   depends_on = [
+#     google_project_service.network_apis
+#   ]
+# }
 
 locals {
   network_apis = [
