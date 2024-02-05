26d4b7e833900adae1cc5cc30c6fbb480a9930d6
ervitis <victor.martinalonso@hrbrain.co.jp>
2024-02-05 22:36:01 +0900

    enable firebase

---
+++ infra/firebase.tf
@@ -0,0 +1,23 @@
+resource "google_project" "default" {
+  name       = "ikigaikintore"
+  project_id = var.project_id
+
+  labels = {
+    "firebase" = "enabled"
+  }
+}
+
+locals {
+  firebase_apis = [
+    "firebase.googleapis.com",
+    "serviceusage.googleapis.com",
+  ]
+}
+
+resource "google_project_service" "firebase_apis" {
+  for_each = toset(local.firebase_apis)
+  project  = google_project.default.project_id
+  service  = each.value
+
+  disable_on_destroy = true
+}
