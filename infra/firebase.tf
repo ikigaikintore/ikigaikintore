70c0bf9592d792e7a99ba76af2c7c8e9ab64f3c3
ervitis <victor.martinalonso@hrbrain.co.jp>
2024-02-05 22:49:55 +0900

    enable apis

--- infra/firebase.tf
+++ infra/firebase.tf
@@ -1,14 +1,3 @@
-resource "google_project" "default" {
-  name       = "ikigaikintore"
-  project_id = var.project_id
-
-  labels = {
-    "firebase" = "enabled"
-  }
-
-  billing_account = var.billing_account
-}
-
 locals {
   firebase_apis = [
     "firebase.googleapis.com",
@@ -18,7 +7,7 @@ locals {
 
 resource "google_project_service" "firebase_apis" {
   for_each = toset(local.firebase_apis)
-  project  = google_project.default.project_id
+  project  = var.project_id
   service  = each.value
 
   disable_on_destroy = true
