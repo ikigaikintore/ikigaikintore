4468c79a4e74f68a9010d2199b9e1df0c8ad0e16
ervitis <victor.martinalonso@hrbrain.co.jp>
2024-02-17 00:14:05 +0900

    apis

--- infra/network.tf
+++ infra/network.tf
@@ -11,3 +11,16 @@ resource "google_vpc_access_connector" "vpc-connector" {
   network       = google_compute_network.vpc-network.name
   ip_cidr_range = "20.8.2.0/28"
 }
+
+locals {
+  network_apis = [
+    "compute.googleapis.com",
+    "vpcaccess.googleapis.com",
+  ]
+}
+
+resource "google_project_service" "network-api-resources" {
+  for_each = toset(local.network_apis)
+  project  = var.project_id
+  service  = each.value
+}
