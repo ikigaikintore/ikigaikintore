14973064fc05d3c00f14c245ed0469e719c17a60
ervitis <victor.martinalonso@hrbrain.co.jp>
2024-02-17 00:23:35 +0900

    delete outputs

--- infra/outputs.tf
+++
@@ -1,7 +0,0 @@
-output "vpc_connector_name" {
-  value = google_vpc_access_connector.vpc-connector.name
-}
-
-output "vpc_connector_ip_range" {
-  value = google_vpc_access_connector.vpc-connector.ip_cidr_range
-}
