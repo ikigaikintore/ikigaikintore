97d05437bfa9732dacf9ded3eb3d42f00cf6163c
ervitis <victor.martinalonso@hrbrain.co.jp>
2024-02-17 22:10:50 +0900

    server file and api

--- backend/cmd/server/main.go
+++ backend/cmd/server/main.go
@@ -3,14 +3,15 @@ package main
 import (
 	"context"
 	"errors"
-	"github.com/ervitis/ikigaikintore/backend/internal/input/grpc"
-	"github.com/rs/cors"
 	"log"
 	"net/http"
 	"os"
 	"os/signal"
 	"syscall"
 	"time"
+
+	"github.com/ervitis/ikigaikintore/backend/internal/input/grpc"
+	"github.com/rs/cors"
 )
 
 type Server interface {
@@ -18,10 +19,26 @@ type Server interface {
 	Shutdown(context.Context) error
 }
 
-func NewServer() Server {
+func NewAppServer() Server {
+	mux := http.NewServeMux()
+	mux.Handle("/v1/weather/", grpc.NewTwirpServer())
+	mux.Handle("/health", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+	}))
 	return &http.Server{
 		Addr:              ":8999",
-		Handler:           cors.Default().Handler(grpc.NewTwirpServer()),
+		Handler:           cors.Default().Handler(mux),
+		ReadTimeout:       5 * time.Second,
+		ReadHeaderTimeout: 10 * time.Second,
+		WriteTimeout:      10 * time.Second,
+		IdleTimeout:       time.Minute,
+	}
+}
+
+func NewFileServer() Server {
+	return &http.Server{
+		Addr:              ":3000",
+		Handler:           cors.Default().Handler(http.StripPrefix("/", http.FileServer(http.Dir("/static")))),
 		ReadTimeout:       5 * time.Second,
 		ReadHeaderTimeout: 10 * time.Second,
 		WriteTimeout:      10 * time.Second,
@@ -33,10 +50,19 @@ func main() {
 	ctx, stop := signal.NotifyContext(context.Background(), os.Kill, os.Interrupt, syscall.SIGTERM)
 	defer stop()
 
-	srv := NewServer()
+	srv := NewAppServer()
+	fileSrv := NewFileServer()
 
 	go func() {
-		if err := srv.ListenAndServe(); err != nil {
+		log.Println("serving app")
+		if err := srv.ListenAndServe(); !errors.Is(err, http.ErrServerClosed) {
+			log.Panic(err)
+		}
+	}()
+
+	go func() {
+		log.Println("serving static files")
+		if err := fileSrv.ListenAndServe(); !errors.Is(err, http.ErrServerClosed) {
 			log.Panic(err)
 		}
 	}()
@@ -45,6 +71,9 @@ func main() {
 	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancel()
 	if err := srv.Shutdown(ctx); err != nil && !errors.Is(err, http.ErrServerClosed) {
+		log.Println(err)
+	}
+	if err := fileSrv.Shutdown(ctx); err != nil && !errors.Is(err, http.ErrServerClosed) {
 		log.Println(err)
 	}
 }
