52f7d6481c73391be250af783ef259589e9707c7
ervitis <victor.martinalonso@hrbrain.co.jp>
2024-03-01 11:30:38 +0900

    add cors in backend

--- backend/cmd/server/main.go
+++ backend/cmd/server/main.go
@@ -5,13 +5,17 @@ import (
 	"errors"
 	"log"
 	"net/http"
+	"net/url"
 	"os"
 	"os/signal"
+	"strings"
 	"syscall"
 	"time"
 
-	"github.com/ervitis/ikigaikintore/backend/internal/input/grpc"
 	"github.com/rs/cors"
+
+	"github.com/ervitis/ikigaikintore/backend/internal/config"
+	"github.com/ervitis/ikigaikintore/backend/internal/input/grpc"
 )
 
 type Server interface {
@@ -19,22 +23,66 @@ type Server interface {
 	Shutdown(context.Context) error
 }
 
-func corsHandler() *cors.Cors {
-	if os.Getenv("ENV") == "dev" {
+func corsHandler(envCfg config.Envs) *cors.Cors {
+	if envCfg.App.IsDev() {
 		return cors.AllowAll()
 	}
-	return cors.Default()
+	return cors.New(
+		cors.Options{
+			AllowedMethods: []string{
+				http.MethodHead,
+				http.MethodGet,
+				http.MethodPost,
+				http.MethodPut,
+				http.MethodPatch,
+				http.MethodDelete,
+				http.MethodConnect,
+				http.MethodOptions,
+				http.MethodTrace,
+			},
+			AllowCredentials: true,
+			AllowedHeaders:   []string{"*"},
+			AllowOriginVaryRequestFunc: func(r *http.Request, origin string) (bool, []string) {
+				if strings.TrimSpace(origin) == "" {
+					return false, []string{}
+				}
+				parsedOrigin, err := url.Parse(origin)
+				if err != nil {
+					return false, []string{}
+				}
+
+				origin = parsedOrigin.Hostname()
+				for _, v := range strings.Split(envCfg.Cors.AllowedDomains, ",") {
+					if v == origin {
+						return true, []string{"Origin"}
+					}
+				}
+
+				return false, []string{}
+			},
+		},
+	)
 }
 
-func NewAppServer() Server {
+func domainAllowed(c *cors.Cors, next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if !c.OriginAllowed(r) {
+			w.WriteHeader(http.StatusMethodNotAllowed)
+			return
+		}
+		next.ServeHTTP(w, r)
+	})
+}
+
+func NewAppServer(envCfg config.Envs) Server {
 	mux := http.NewServeMux()
-	mux.Handle("/v1/weather/", grpc.NewTwirpServer())
+	mux.Handle("/v1/weather/", domainAllowed(corsHandler(envCfg), grpc.NewTwirpServer()))
 	mux.Handle("/health", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		w.WriteHeader(http.StatusOK)
 	}))
 	return &http.Server{
 		Addr:              ":8080",
-		Handler:           corsHandler().Handler(mux),
+		Handler:           mux,
 		ReadTimeout:       5 * time.Second,
 		ReadHeaderTimeout: 10 * time.Second,
 		WriteTimeout:      10 * time.Second,
@@ -46,7 +94,9 @@ func main() {
 	ctx, stop := signal.NotifyContext(context.Background(), os.Kill, os.Interrupt, syscall.SIGTERM)
 	defer stop()
 
-	srv := NewAppServer()
+	envCfg := config.Load()
+
+	srv := NewAppServer(envCfg)
 
 	go func() {
 		log.Println("serving app")
--- backend/go.mod
+++ backend/go.mod
@@ -18,6 +18,7 @@ require (
 	github.com/google/uuid v1.4.0 // indirect
 	github.com/invopop/yaml v0.1.0 // indirect
 	github.com/josharian/intern v1.0.0 // indirect
+	github.com/kelseyhightower/envconfig v1.4.0 // indirect
 	github.com/mailru/easyjson v0.7.7 // indirect
 	github.com/mohae/deepcopy v0.0.0-20170929034955-c48cc78d4826 // indirect
 	github.com/perimeterx/marshmallow v1.1.4 // indirect
