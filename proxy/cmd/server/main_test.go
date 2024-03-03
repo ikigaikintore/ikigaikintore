5d629e6937772d35080b1a2a341cbcd983efd19b
ervitis <victor.martinalonso@hrbrain.co.jp>
2024-03-03 18:30:05 +0900

    test ip rate limit

--- proxy/cmd/server/main_test.go
+++ proxy/cmd/server/main_test.go
@@ -1,7 +1,10 @@
 package main
 
 import (
+	"fmt"
 	"golang.org/x/time/rate"
+	"net/http"
+	"net/http/httptest"
 	"sync"
 	"testing"
 )
@@ -10,7 +13,6 @@ func Test_ipRateLimiter_ipAddress(t *testing.T) {
 	type fields struct {
 		ips        map[string]*rate.Limiter
 		tokenPerIp int
-		mtx        sync.Locker
 		rl         rate.Limit
 	}
 	type args struct {
@@ -27,7 +29,6 @@ func Test_ipRateLimiter_ipAddress(t *testing.T) {
 			fields: fields{
 				ips:        make(map[string]*rate.Limiter),
 				tokenPerIp: 10,
-				mtx:        &sync.Mutex{},
 				rl:         1,
 			},
 			args: args{
@@ -40,7 +41,6 @@ func Test_ipRateLimiter_ipAddress(t *testing.T) {
 			fields: fields{
 				ips:        make(map[string]*rate.Limiter),
 				tokenPerIp: 10,
-				mtx:        &sync.Mutex{},
 				rl:         1,
 			},
 			args: args{
@@ -55,11 +55,78 @@ func Test_ipRateLimiter_ipAddress(t *testing.T) {
 			ir := &ipRateLimiter{
 				ips:        tt.fields.ips,
 				tokenPerIp: tt.fields.tokenPerIp,
-				mtx:        tt.fields.mtx,
+				mtx:        sync.RWMutex{},
 				rl:         tt.fields.rl,
 			}
 			if got := ir.ipAddress(tt.args.ip); got != tt.want {
 				t.Errorf("ipAddress() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_ipRateLimiterMid(t *testing.T) {
+	type args struct {
+		cl *ipRateLimiter
+	}
+	tests := []struct {
+		name    string
+		n       int
+		args    args
+		checker func(*httptest.ResponseRecorder, *http.Request) error
+	}{
+		{
+			name: "not block",
+			args: args{
+				cl: newIpRateLimiter(),
+			},
+			n: 1,
+			checker: func(rr *httptest.ResponseRecorder, req *http.Request) error {
+				if rr.Code != http.StatusOK {
+					return fmt.Errorf("not 200 code")
+				}
+				return nil
+			},
+		},
+		{
+			name: "block",
+			args: args{
+				cl: newIpRateLimiter(withLimit(1), withTokensPerIp(1)),
+			},
+			n: 10,
+			checker: func(rr *httptest.ResponseRecorder, req *http.Request) error {
+				if rr.Code == http.StatusTooManyRequests {
+					fmt.Println("ip blocked")
+				}
+				return nil
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if tt.checker == nil {
+				t.Errorf("checkere is nil")
+			}
+			next := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+				w.WriteHeader(http.StatusOK)
+			})
+			mid := ipRateLimiterMid(tt.args.cl)(next)
+
+			req, _ := http.NewRequest(http.MethodGet, "http://example.com", nil)
+			req.RemoteAddr = "128.2.2.2:7777"
+
+			for i := 0; i < tt.n; i++ {
+				rr := httptest.NewRecorder()
+				if i == 0 {
+					if rr.Code != http.StatusOK {
+						t.Errorf("code not 200")
+					}
+				}
+
+				mid.ServeHTTP(rr, req)
+				if err := tt.checker(rr, req); err != nil {
+					t.Error(err)
+				}
 			}
 		})
 	}
