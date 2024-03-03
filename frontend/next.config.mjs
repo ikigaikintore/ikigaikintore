504b0cdb58860525aaf11ae4ab553727d34edd0a
ervitis <victor.martinalonso@hrbrain.co.jp>
2024-03-04 00:20:56 +0900

    add headers

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -2,3 +2,24 @@ export const compiler = {
     styledComponents: true,
 };
 export const output = "export";
+export async const headers = () => {
+    return [
+        {
+            source: "/",
+            headers: [
+                {
+                    key: "Access-Control-Allow-Origin",
+                    value: "*",
+                },
+                {
+                    key: "Access-Control-Allow-Methods",
+                    value: "GET, POST, PUT, DELETE, OPTIONS",
+                },
+                {
+                    key: "Access-Control-Allow-Headers",
+                    value: "Content-Type, Authorization",
+                },
+            ],
+        },
+    ];
+}
