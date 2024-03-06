4ce3f1cbeed025628bae6d953c33bd1e9b7c8b79
ervitis <victor.martinalonso@hrbrain.co.jp>
2024-03-06 23:22:29 +0900

    next using firefunctions

--- frontend/src/auth/init.ts
+++ frontend/src/auth/init.ts
@@ -1,7 +1,10 @@
+import { connectFunctionsEmulator, getFunctions } from "@firebase/functions"
 import { initializeApp } from "firebase/app"
 import { Auth, getAuth } from "firebase/auth"
 
 import { firebaseConfig } from "./config"
+
+import { useConfig } from "@/src/config/use-config"
 
 const app = initializeApp(firebaseConfig)
 const auth: Auth = getAuth(app)
@@ -13,6 +16,12 @@ const token = async () => {
     console.error(`Error getting token: ${err}`)
     return ""
   }
+}
+
+const functions = getFunctions(app)
+if (useConfig().isLocal()) {
+  const { host, port } = useConfig().functionsEmulatorHost()
+  connectFunctionsEmulator(functions, host, port)
 }
 
 export { app, auth, token }
--- frontend/src/config/use-config.ts
+++ frontend/src/config/use-config.ts
@@ -1,10 +1,14 @@
 export const useConfig = () => {
-  
+
   const isLocal = () => process.env.NEXT_PUBLIC_ENVIRONMENT === "local"
   const emulatorHost = () => process.env.NEXT_PUBLIC_FIREBASE_EMULATOR_HOST || "http://localhost:9099"
+  const functionsEmulatorHost = () => {
+    return { host: "localhost", port: 5001 }
+  }
   const emails = process.env.NEXT_PUBLIC_USER_AUTH?.split(",") || []
-  
+
   return {
+    functionsEmulatorHost,
     emails,
     isLocal,
     emulatorHost,
