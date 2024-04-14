7827c713564c0b6472c283ecfb465f198e28f539
ervitis <vitomarti@gmail.com>
2024-04-14 21:31:33 +0900

    not use auth and fix path

--- frontend/firebaseFunctions.js
+++ frontend/firebaseFunctions.js
@@ -2,16 +2,16 @@ const { https } = require("firebase-functions")
 const next = require("next")
 const { join } = require("path")
 
-const nextjsDistDir = join("src", require("./src/next.config.js").distDir)
+const nextjsDistDir = join("src", require("./next.config.js").distDir)
 
 const nextjsServer = next({
-  dev: true,
-  conf: {
-    distDir: nextjsDistDir,
-  },
+    dev: true,
+    conf: {
+        distDir: nextjsDistDir,
+    },
 })
 const nextjsHandle = nextjsServer.getRequestHandler()
 
 exports.nextjsFunc = https.onRequest((req, res) => {
-  return nextjsServer.prepare().then(() => nextjsHandle(req, res))
+    return nextjsServer.prepare().then(() => nextjsHandle(req, res))
 })
